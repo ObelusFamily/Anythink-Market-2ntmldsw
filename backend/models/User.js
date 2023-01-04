ff69427e7a4ee7ca1466e39f22d193e8abe11d6c
M S S PRAKASH YASHWANTH <78893920+PrakashYashwanth@users.noreply.github.com>
2023-01-04 12:53:30 +0000

    feature: adding isVerified flag to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,7 +32,8 @@ var UserSchema = new mongoose.Schema(
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
-    salt: String
+    salt: String,
+    isVerified: Boolean
   },
   { timestamps: true }
 );
@@ -85,7 +86,8 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     bio: this.bio,
     image:
       this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
-    following: user ? user.isFollowing(this._id) : false
+    following: user ? user.isFollowing(this._id) : false,
+    isVerified:false
   };
 };
 
